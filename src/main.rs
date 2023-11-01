bbca83dc548b26663f5a4727feef1971e6680247
johannes sandjaja <johannes@johanness-MacBook-Pro.local>
2023-11-01 09:39:22 +0700

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,26 @@ use std::env;
 // use serde_bencode
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    // if encoded_value.chars().next().unwrap().is_digit(10) {
-    //     // Example: "5:hello" -> "hello"
-    //     let colon_index = encoded_value.find(':').unwrap();
-    //     let number_string = &encoded_value[..colon_index];
-    //     let number = number_string.parse::<i64>().unwrap();
-    //     let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-    //     return serde_json::Value::String(string.to_string());
-    // } else {
-    //     panic!("Unhandled encoded value: {}", encoded_value)
-    // }
-
-    if let Some((_, right)) = encoded_value.split_once(':') {
-        serde_json::Value::String(right.to_string())
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+fn decode_bencoded_value(encoded_value: &str) -> String {
+    match encoded_value {
+        // integer
+        value if value.starts_with('i') && value.ends_with('e') => {
+            let x = value
+                .get(1..value.len() - 1)
+                .unwrap_or_else(|| panic!("Error slicing the integer"))
+                .parse::<i32>()
+                .unwrap_or_else(|e| panic!("Error parsing integer {}", e));
+            x.to_string()
+        }
+        // string
+        val if val.contains(':') => {
+            if let Some((_, right)) = val.split_once(':') {
+                right.to_string()
+            } else {
+                panic!("[string]Unhandled encoded value: {}", val)
+            }
+        }
+        _ => panic!("unknown value"),
     }
 }
 
